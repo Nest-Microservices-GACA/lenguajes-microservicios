a4e79838bd441e55214681f78b8ae3c5754b1f56
AngelMaga23 <angelmaga23@outlook.com>
2024-11-20 17:59:38 -0600

    Change MessagePattern

--- src/languages/languages.controller.ts
+++ src/languages/languages.controller.ts
@@ -8,27 +8,27 @@ import { UpdateLanguageDto } from './dto/update-language.dto';
 export class LanguagesController {
   constructor(private readonly languagesService: LanguagesService) {}
 
-  @MessagePattern('createLanguage')
+  @MessagePattern('lenguaje.create')
   create(@Payload() createLanguageDto: CreateLanguageDto) {
     return this.languagesService.create(createLanguageDto);
   }
 
-  @MessagePattern('findAllLanguages')
+  @MessagePattern('lenguaje.findAll')
   findAll() {
     return this.languagesService.findAll();
   }
 
-  @MessagePattern('findOneLanguage')
+  @MessagePattern('lenguaje.findOne')
   findOne(@Payload() id: number) {
     return this.languagesService.findOne(id);
   }
 
-  @MessagePattern('updateLanguage')
+  @MessagePattern('lenguaje.update')
   update(@Payload() updateLanguageDto: UpdateLanguageDto) {
     return this.languagesService.update(updateLanguageDto.idu_lenguaje, updateLanguageDto);
   }
 
-  @MessagePattern('removeLanguage')
+  @MessagePattern('lenguaje.remove')
   remove(@Payload() id: number) {
     return this.languagesService.remove(id);
   }
